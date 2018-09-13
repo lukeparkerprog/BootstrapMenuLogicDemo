2c3e96b32f873080d8d05b35837d6eb765444d76
Luke Parker <lukeoliverparker@gmail.com>
2018-09-14 00:05:55 +1000

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuLogicTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Drop()
+        {
+            return View();
+        }
+    }
+}
